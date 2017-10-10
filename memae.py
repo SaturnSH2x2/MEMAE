0ef6edb1845bc1790b6dadb4a4dd6cabb903ad57
A A <helpithinkihavehermes@gmail.com>
2017-10-10 14:36:06 -0400

    added zipfile support to character extraction

--- memae.py
+++ memae.py
@@ -2,8 +2,23 @@ import os
 import sys
 import json
 import subprocess
+import zipfile
+
+# I come up with the most creative names, don't I
+def getSlashIndex(path):
+    cIndex = 0
+    for char in path:
+        if char == "/":
+            return cIndex
+            
+        cIndex += 1
+            
+    return -1
 
 def main():
+    if not os.path.exists("install"):
+        os.mkdir("install")
+
     try:
         with open("config.json", "r") as c:
             config = json.load(c)
@@ -38,6 +53,7 @@ def main():
     lnIndex = 0
     
     installedChars = 0
+    failedChars = 0
         
     try:
         charList = os.listdir(os.path.join(".", "install", "chars"))
@@ -62,16 +78,50 @@ def main():
                 
         sys.stdout.write("Installing {}...    ".format(char))
         
-        # TODO: add zip file support
+        basename = char
         try:
-            with open(os.path.join("install", "chars", char, "{}.json".format(char)), "r") as cc:
+            jsPath = os.path.join("install", "chars", char, "memae.json")
+            with open(jsPath) as cc:
                 charConfig = json.load(cc)
                 charLine = charConfig["line"]
                 cc.close()
         except FileNotFoundError:
+            sys.stdout.write("JSON File not found...  ")
             charLine = char
             
-        os.rename(os.path.join("install", "chars", char), os.path.join(installPath, "chars", char))
+        if char.endswith(".zip"):
+            basename = char[:len(char) - 4]
+            z = zipfile.ZipFile(os.path.join("install", "chars", char))
+            extractToDirectory = True
+            print(z.namelist())
+            for entry in z.namelist():
+                if '{}.def'.format(basename) in entry and "/" in entry:   # the .def file is stored in a subdirectory
+                    extractToDirectory = False
+                    path = os.path.join("install", "chars")
+                    break
+                    
+            charLine = basename
+            for entry in z.namelist():
+                if "memae.json" in entry:
+                    js = z.read(entry)
+                    js = js.decode("utf-8")
+                    print(js)
+                    jsData = json.loads(js)
+                    charLine = jsData["line"]
+                    
+            if extractToDirectory:
+                os.mkdir(os.path.join("install", "chars", basename))
+                path = os.path.join("install", "chars", basename)
+                
+            z.extractall(path = path)
+            z.close()
+            
+        try:
+            os.rename(os.path.join("install", "chars", basename), os.path.join(installPath, "chars", basename))
+        except FileExistsError:
+            print("File already exists, skipping...")
+            failedChars += 1
+            continue
         if not charLine.endswith("\n"):
             selectDef[lnIndex] = charLine + "\n"
         else:
@@ -79,7 +129,7 @@ def main():
         
         sys.stdout.write("Done.\n")
         installedChars += 1
-    
+    0
     sys.stdout.write("Writing to \"select.def\"...   ")
     
     lnIndex = 0
@@ -89,11 +139,15 @@ def main():
         sd.close()
         
     sys.stdout.write("Done.\n")
+    print("\n\n--------------------------------")
+    print("|           RESULTS            |")
+    print("--------------------------------")
     print("{} characters installed successfully.".format(installedChars))
+    print("{} failed character installations.".format(failedChars))
     print("Now running MUGEN...")
     
     # thanks:  https://stackoverflow.com/questions/21406887/subprocess-changing-directory
-    subprocess.Popen("cd {} && .\\mugen.exe".format(installPath), shell = True)
+    #subprocess.Popen("cd {} && .\\mugen.exe".format(installPath), shell = True)
     sys.exit()
     
 if __name__ == "__main__":
